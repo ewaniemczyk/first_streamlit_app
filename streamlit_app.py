5c8b3f57314f30db73a9cb4800a4700f6e6b3a70
ewaniemczyk <81054151+ewaniemczyk@users.noreply.github.com>
2023-03-06 12:55:05 +0100

    Update streamlit_app.py
    changed get_response to get

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,5 @@ streamlit.dataframe(fruit_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response=requests.get_response("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
