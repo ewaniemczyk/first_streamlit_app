d1cdb96e0f35e0dd87624947322ecec528a66f6c
ewaniemczyk <81054151+ewaniemczyk@users.noreply.github.com>
2023-03-07 21:55:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ def get_fruityvice_data(this_fruit_choice):
   return fruityvice_normalized
 
 #New Section to display Fruityvice API response
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header("View our fruit list! Add Your favourites!")
 try:
   fruit_choice=streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
@@ -86,12 +86,14 @@ streamlit.text("the fruit load list contains: ")
 def get_fruit_load_list():
   with cnx.cursor() as my_cur:
     my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+    cnx.close()
     return my_cur.fetchall()
 
 #Add button  to load the fruit  - I had to commented as later the same button 'get fruit load list' is on the end
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get fruit list'):
   cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data=get_fruit_load_list()
+  cnx.close()
   streamlit.dataframe(my_data)
 
 #function to load snowflake
@@ -105,5 +107,6 @@ add_my_fruit=streamlit.text_input('What fruit would You like to add')
 if streamlit.button('Add a fruit to the list'):
   cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
   streamlit.text(insert_row_snowflake(add_my_fruit))
+  cnx.close()
   streamlit.dataframe(get_fruit_load_list())
   
