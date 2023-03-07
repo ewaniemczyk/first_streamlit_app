da0f5e9906177f46f943a72b82624bfa2744ce51
ewaniemczyk <81054151+ewaniemczyk@users.noreply.github.com>
2023-03-07 20:36:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,8 @@ fruit_choice=streamlit.text_input('What fruit would you like information about?'
 streamlit.write('The user entered', fruit_choice)
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
+streamlit.stop()
+
 import snowflake.connector
 my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur=my_cnx.cursor()
